fdab03705cf96f55abb2a26b0597a78b8520beca
AeneasRUSLIN <ruslin.aeneas@gmail.com>
2022-10-19 11:54:44 +0200

    assignment6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Task} from "./model/task";
+import {CrudService} from "./service/crud.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TodoList';
+  show = true;
+
+  taskObj : Task = new Task();
+  taskArr : Task[] = [];
+
+  addTaskValueDesc : string = '';
+  addTaskValueSum : string = '';
+
+  constructor(private crudService : CrudService) { }
+
+  ngOnInit(): void {
+    this.addTaskValueDesc = '';
+    this.addTaskValueSum = '';
+    this.taskObj = new Task();
+    this.taskArr = [];
+    this.getAllTask();
+  }
+
+  getAllTask() {
+    this.crudService.getAllTask().subscribe(res => {
+      this.taskArr = res;
+    });
+  }
+
+  addTask() {
+    this.taskObj.task_name = this.addTaskValueDesc; //changed
+    this.taskObj.task_sum = this.addTaskValueSum;
+    this.crudService.addTask(this.taskObj).subscribe( res=> {
+      this.ngOnInit();
+    })
+  }
+
+  deleteTask(task : Task) {
+    this.crudService.deleteTask(task).subscribe(res => {
+      this.ngOnInit();
+    })
+  }
+
 }
